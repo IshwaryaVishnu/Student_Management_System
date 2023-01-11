fcdefb75ff7f6ae36cc45714cf2c5d7f9d4ba14b
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-01-11 09:48:23 +0100

    spring

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,14 +1,18 @@
 package se.lexicon;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.ComponentScanConfig;
+import se.lexicon.data_access.StudentDao;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        StudentDao studentDao = context.getBean(StudentDao.class);
 
+    }
 }
--- src/main/java/se/lexicon/config/ComponentScanConfig.java
+++ src/main/java/se/lexicon/config/ComponentScanConfig.java
@@ -1,16 +1,10 @@
 package se.lexicon.config;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import se.lexicon.data_access.StudentDao;
 
 @Configuration
 @ComponentScan(basePackages = "se.lexicon")
 
 public class ComponentScanConfig {
 
-    AnnotationConfigApplicationContext context =
-            new AnnotationConfigApplicationContext(ComponentScanConfig.class);
-    StudentDao studentDao = context.getBean(StudentDao.class);
 }
