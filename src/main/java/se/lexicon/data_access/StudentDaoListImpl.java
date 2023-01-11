d659e7a812e915c7962d76f2b62646733ad6a5fa
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-01-11 16:05:27 +0100

    Update

--- src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -2,24 +2,44 @@ package se.lexicon.data_access;
 import org.springframework.stereotype.Component;
 import se.lexicon.model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 @Component
 public class StudentDaoListImpl  implements StudentDao{
 
+    private List<Student> students = new ArrayList<>();
+
 
     public Student find(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
         return null;
     }
+
 
     public Student save(Student student) {
-        return null;
+        students.add(student);
+        return student;
     }
 
+
     public List<Student> findAll() {
-        return null;
+
+        return new ArrayList<>();
     }
 
     public void delete(int id) {
+            for (int i = 0; i < students.size(); i++) {
+                if (students.get(i).getId() == id) {
+                    students.remove(i);
+                    break;
+                }
+            }
+        }
+
 
     }
-}
+
--- src/main/java/se/lexicon/service/StudentManagementConsoleImpl.java
+++ src/main/java/se/lexicon/service/StudentManagementConsoleImpl.java
@@ -8,17 +8,7 @@ import se.lexicon.util.UserInputService;
 import java.util.ArrayList;
 import java.util.List;
 @Component
-public class StudentManagementConsoleImpl implements StudentManagement{
-
- // Constructor injection:
-  /*  private final UserInputService scannerService;
-    private final StudentDao studentDao;
-
-    @Autowired
-    public StudentManagementConsoleImpl(UserInputService scannerService, StudentDao studentDao) {
-        this.scannerService = scannerService;
-        this.studentDao = studentDao;
-    }*/
+public class StudentManagementConsoleImpl implements StudentManagement {
 
     //Field injection:
     @Autowired
@@ -28,49 +18,35 @@ public class StudentManagementConsoleImpl implements StudentManagement{
 
     private List<Student> students = new ArrayList<>();
 
-       public Student create() {
-        return (Student) students;
+    @Override
+    public Student create() {
+        return null;
     }
 
+    @Override
     public Student save(Student student) {
-        students.add(student);
-        return student;
+        return null;
     }
 
+    @Override
     public Student find(int id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                return student;
-            }
-        }
         return null;
     }
 
-
+    @Override
     public Student remove(int id) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getId() == id) {
-                Student removedStudent = students.get(i);
-                students.remove(i);
-                return removedStudent;
-            }
-        }
         return null;
     }
 
-
+    @Override
     public List<Student> findAll() {
-        return new ArrayList<>();
-    }
-
-    public Student edit(Student student) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getId() == student.getId()) {
-                students.set(i, student);
-                return student;
-            }
-        }
         return null;
     }
+
+    @Override
+    public Student edit(Student student) {
+        return null;
     }
+}
+
 
